fbf7360e06f8f800cf83591787791981bbd46d9d
sleeper-g <green.sleeper1@gmail.com>
2025-04-18 19:22:05 +0300

    Initial commit

---
+++ src/js/app.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.querySelector('.collapse__btn');
+  const content = document.querySelector('.collapse__content');
+
+  btn.addEventListener('click', () => {
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + 'px';
+    }
+  });
+});
+
